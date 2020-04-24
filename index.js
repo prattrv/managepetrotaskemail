1746e3fa82ec8aaa4cb130ab5f901153e13a2b5f
Prathamesh Vaidya <prathameshrvaidya@gmail.com>
2020-04-23 22:55:51 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+const app = express();
+
+app.use(fileUpload());
+
+app.get("/email", (req, res) => {
+  let transporter = nodemailer.createTransport({
+    service: process.env.SERVICE,
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PWD,
+    },
+  });
+
+  let mailOptions = {
+    from: process.env.EMAIL,
+    to: ["prathameshrvaidya@gmail.com"],
+    subject: "Database Backup",
+    text: "Please find the attached CSV file",
+    attachments: [
+      {
+        filename: req.files.customers.name,
+        content: req.files.customers.data,
+      },
+    ],
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(400).json({
+        error: errorHandler.getErrorMessage(error),
+      });
+    }
+
+    res.status(200).json({
+      message: "Mail sent!",
+    });
+  });
+});
+
+const server = app.listen(4000, () => {
+  console.log("Server is running on port 4000");
+});
