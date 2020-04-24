4d244afdabf170ac26650ce9256ce943d24d76c2
Prathamesh Vaidya <prathameshrvaidya@gmail.com>
2020-04-23 23:24:33 -0700

    email debug patch

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
 
 app.post("/email", (req, res) => {
   console.log("About to send email");
-  console.log("Data", req.body.customers);
+  console.log("Data", req);
   let transporter = nodemailer.createTransport({
     service: process.env.SERVICE,
     auth: {
